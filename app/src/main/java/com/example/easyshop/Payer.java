5d1c66d3a925b65bf9374602dbf20672e75e82f2
gtandia <tandia.g@live.fr>
2019-12-03 16:24:55 +0100

    Merge branch 'master' of https://github.com/hugoamiach/easyShop
    # Conflicts:
    #	app/src/main/java/com/example/easyshop/MainActivity.java

---
+++ app/src/main/java/com/example/easyshop/Payer.java
@@ -0,0 +1,20 @@
+package com.example.easyshop;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Payer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.payer);
+        Bundle bundle = getIntent().getExtras();
+        Double str = bundle.getDouble("panier", 0);
+        TextView textView = (TextView) findViewById(R.id.panier);
+        textView.setText(String.valueOf(str));
+    }
+}
